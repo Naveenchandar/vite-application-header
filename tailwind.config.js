725d6c7839205665407c9f076787dc37766020c1
naveenchandar <naveenchandece@gmail.com>
2024-11-16 20:27:35 +0530

    tailwind css config added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,jsx}"],
+  content: [
+    "./src/**/*.{html,jsx}",
+    './index.html'
+  ],
   theme: {
     extend: {},
   },
